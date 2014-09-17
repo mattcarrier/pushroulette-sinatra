60d570b07f12ccd9a52619fbdefea49731cd9d30
Derek Pavao <dpavao@astonish.com>
2014-09-17 16:20:40 -0400

    Got the sound cloud client working as a instance variable

--- app/routes/base-route.rb
+++ app/routes/base-route.rb
@@ -4,9 +4,10 @@ module Pushroulette
   class BaseRoute < Sinatra::Base
 
 
-    $client =  SoundCloud.new(:client_id => 'cdbefc208d1db7a07c5af0e27e10b403')
-
-
+    def initialize(app)
+      super
+      @client = SoundCloud.new(:client_id => 'cdbefc208d1db7a07c5af0e27e10b403')
+    end
 
     def playClip(clip, deleteAfterPlay=false)
       played = false
@@ -30,7 +31,7 @@ module Pushroulette
     def downloadClips(num=1)
       i = 0
       while i < num do
-        tracks = $client.get('/tracks', :q => 'downloadable', :limit => 50, :offset => [*0..8001].sample)
+        tracks = @client.get('/tracks', :q => 'downloadable', :limit => 50, :offset => [*0..8001].sample)
         for track in tracks
           if track.original_content_size < 10000000 and !track.download_url.nil?
             open('/etc/pushroulette/library/' + track.title + '.' + track.original_format, 'wb') do |file|
