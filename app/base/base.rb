c76e641dcbb2297adf0e2ed0a5add32f0e727fee
Matt Carrier <mcarrieruri@gmail.com>
2014-09-22 23:20:20 -0400

    semi-working genres

--- app/base/base.rb
+++ app/base/base.rb
@@ -23,42 +23,77 @@ module Pushroulette
     end
 
     def pushrouletteConfig(config_key)
-      @config['pushroulette']
+      @config['pushroulette'][config_key]
     end
 
     def playClip(clip, deleteAfterPlay=false, *genre)
-      played = false
-      songs = 0;
-      if (genre.any?)
-        dir = "/etc/pushroulette/library/#{genre.first}/"
-      elsif
-        dir = "/etc/pushroulette/library/"
-      end
-      while !played do
-        file = clip.nil? ? Dir.glob("#{dir}pushroulette_*.mp3").sample : clip
-        played = system "avplay -autoexit -nodisp #{file}" || !clip.nil?
-
-        if deleteAfterPlay
-          File.delete(file)
+      Thread.new {
+        played = false
+        songs = 0;
+        if (genre.any?)
+          dir = "/etc/pushroulette/library/#{genre.first}/"
+        else
+          dir = "/etc/pushroulette/library/"
         end
-        songs += 1
-      end
+        while !played do
+          file = clip.nil? ? Dir.glob("#{dir}pushroulette_*.mp3").sample : clip
+          played = system "avplay -autoexit -nodisp #{file}" || !clip.nil?
 
-      if clip.nil?
-        downloadClips(songs)
-      end
+          if deleteAfterPlay
+            File.delete(file)
+          end
+          songs += 1
+        end
+
+        if clip.nil?
+          downloadClips(songs)
+        end
+      }
     end
 
 
     def downloadClips(num=1, *genre)
+      # @genres: cache for number of songs per genre
+      if (@genres.nil?)
+        @genres = Hash.new
+      end
+
+      # retrieve the number of songs per genre
+      totalClips = 1000
+      if (genre.any?)
+        totalClips = @genres[genre.first]
+
+        # find the number of songs per genre if not cached yet
+        if (totalClips.nil?)
+          begin
+            totalClips = totalClips.nil? ? 1000 : (totalClips / 2).to_i
+            puts totalClips
+            tracks = @client.get('/tracks', :filter => 'downloadable', :limit => 50, :genres => genre.first.downcase, :offset => totalClips - 50)
+          end while !tracks.any?
+
+          if (1000 != totalClips and 50 == tracks.length)
+            begin
+              totalClips += 50
+              puts totalClips
+              tracks = @client.get('/tracks', :filter => 'downloadable', :limit => 50, :genres => genre.first.downcase, :offset => totalClips - 50)
+              puts "tracks: #{tracks.length}"
+            end while 0 < tracks.length and 50 > tracks.length
+          end
+
+          totalClips += tracks.length
+          totalClips -= 50
+          @genres[genre.first] = totalClips
+        end
+      end
+
       i = 0
       while i < num do
         if (genre.any?)
           dir = "/etc/pushroulette/library/#{genre.first}/"
-          tracks = @client.get('/tracks', :q => 'downloadable', :limit => 50, :genres => genre.first, :offset => [*0..8001].sample)
-        elsif
+          tracks = @client.get('/tracks', :filter => 'downloadable', :limit => 50, :genres => genre.first.downcase, :offset => [*0..totalClips].sample).shuffle
+        else
           dir = "/etc/pushroulette/library/"
-          tracks = @client.get('/tracks', :q => 'downloadable', :limit => 50, :offset => [*0..8001].sample)
+          tracks = @client.get('/tracks', :filter => 'downloadable', :limit => 50, :offset => [*0..8000].sample).shuffle
         end
         for track in tracks
           if track.original_content_size < 10000000 and !track.download_url.nil?
--- app/routes/github.rb
+++ app/routes/github.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'fileutils'
 require './app/base/base'
 
 module Pushroulette
@@ -8,22 +9,27 @@ module Pushroulette
       request.body.rewind  # in case someone already read it
       data = JSON.parse request.body.read
       puts data
-      playClip(nil, true)
+      user = @users[data['pusher']['name']]
+      playClip(nil, true, user.nil? ? nil : user[genre])
     end
 
-    def initialize(app)
-      super
-      @users = githubConfig('users')
-      for user in @users
-        genreClipDir = "/etc/pushroulette/library/#{user[genre]}"
-        if (!(Dir.exists?(genreClipDir)) || !(Dir.glob("#{genreClipDir}/pushroulette_*.mp3").any?))
-          downloadClips(5, user[genre])
+    post '/github/initialize' do
+      Thread.new {
+        @users = githubConfig('users')
+        @users.each do |username, props|
+          puts "username: #{username}"
+          puts "props: #{props}"
+          genreClipDir = "/etc/pushroulette/library/#{props['genre']}"
+          FileUtils::mkdir_p(genreClipDir)
+          if 5 > Dir.glob("#{genreClipDir}/pushroulette_*.mp3").length
+            downloadClips(5, props['genre'])
+          end
         end
-      end
 
-      if !(Dir.glob("/etc/pushroulette/library/pushroulette_*.mp3").any?)
-        downloadClips(5)
-      end
+        if 5 > Dir.glob("/etc/pushroulette/library/pushroulette_*.mp3").length
+          downloadClips(5)
+        end
+      }
     end
 
     def genreForUser(user)
