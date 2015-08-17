0a095b5da2a35c5245896d0a422de69ec916cc8d
Matt Carrier <mcarrier@astonish.com>
2015-08-17 16:09:59 -0400

    Not nul but nil

--- app/routes/github.rb
+++ app/routes/github.rb
@@ -24,7 +24,7 @@ module Pushroulette
           speak("#{username}, pushed to #{repository}")
         end
       end
-      playClip(user.nul? ? nil : user['clip'], true, user.nil? ? nil : user['genre'], true)
+      playClip(user.nil? ? nil : user['clip'], true, user.nil? ? nil : user['genre'], true)
     end
 
     def githubConfig(config_key)
